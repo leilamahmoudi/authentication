1e0cccb20ad09b6b7e00136f1d7905fe9cb496a0
leilamahmoudi <leila.mahmoudi9169@gmail.com>
2022-05-31 10:51:02 +0200

    added routes

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 
 const app = express();
 
+//Routes
+app.use("/", require("./routes/index"));
+
 const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, console.log(`Server start on port ${PORT}`));

bd05fdfdf0330345515b424b9cdb13f4ce068b6d
leilamahmoudi <leila.mahmoudi9169@gmail.com>
2022-06-01 17:39:18 +0200

    added logout user's name

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const { ensureAuthenticated } = require("../config/auth");
 
 // Welcome Page
 router.get("/", (req, res) => res.render("Welcome"));
 
 // Dashboard
-router.get("/dashboard", (req, res) => res.render("dashboard"));
+router.get("/dashboard", ensureAuthenticated, (req, res) =>
+  res.render("dashboard", {
+    user: req.user,
+  })
+);
 
 module.exports = router;
